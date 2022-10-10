10b4a4d51c805fd9f4e82f4c7f8e2bcd1490defc
Olga <80950029+olgabelka81@users.noreply.github.com>
2022-10-10 20:13:42 +0300

    Добавляет  необходимые функции для создания массива

--- js/main.js
+++ js/main.js
@@ -18,3 +18,56 @@ function checkStringLength (string, length) {
   return string.length <= length;
 }
 checkStringLength ('Привет!', 20);
+
+// module4-task1
+//const ID = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+
+const DESCRIPTION = [
+  'Дисплей',
+  'Окружающая среда',
+  'Приоритет',
+  'Гордость образования',
+  'Романтика',
+  'Смелость',
+  'Поражение штормов',
+  'Источник тайны',
+  'Осень',
+  'Неудачное падение',
+  'Уверенность в культуре',
+  'Дым блеска',
+  'Влияние',
+  'Последствия',
+  'Древние трофеи',
+  'Бизнес',
+  'Забавный контракт',
+  'Внешность',
+  'Приоритет жизни',
+  'Шторм',
+  'Город эмоций',
+  'Проблемы реальности',
+  'Сюрприз образования',
+  'Благотворительность',
+  'Покинутое здравомыслие'
+];
+const COUNT_CARDS_PHOTO = 25;
+const getCardPhoto = () => ({
+  // const IdGenerator = getRandomPositiveInteger(0, Id.length - 1);
+
+  id: 1,
+  url : '',
+  description : DESCRIPTION[getRandomPositiveInteger(0, DESCRIPTION.length - 1)],
+  likes : getRandomPositiveInteger(15, 200),
+  comment : getRandomPositiveInteger(0, 200),
+});
+
+const getPhotos = Array.from({length: COUNT_CARDS_PHOTO }, getCardPhoto);
+console.log (
+  getPhotos
+);
+
+/*id : 1, //число — идентификатор опубликованной фотографии. Это число от 1 до 25. Идентификаторы не должны повторяться.
+  url : 'photos/i.jpg', //строка — адрес картинки
+  description : ' ', // строка — описание фотографии
+  likes : 0, //число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
+  comments : 0 //число — количество комментариев, оставленных другими пользователями к этой фотографии. Случайное число от 0 до 200.*/
+
