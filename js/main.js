e84f156d9cfbf8506c64ef50d00a6fa2683580af
Olga <80950029+olgabelka81@users.noreply.github.com>
2022-10-19 16:46:57 +0300

    Добавляет функцию отрисовки миниатюр

--- js/main.js
+++ js/main.js
@@ -1,2 +1,1 @@
-import {similarListFragment} from './picture.js';
-
+import {getPictures} from './picture.js';
--- js/picture.js
+++ js/picture.js
@@ -2,20 +2,18 @@ import {createPhotos} from './cards-photos.js';
 
 const pictures = document.querySelector('.pictures');
 
-const similarListElement = document.createElement('section');
-similarListElement.classList.add('picture__list');
-
 const similarPictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const getPictures = function () {
+  const similarListFragment = document.createDocumentFragment();
+  createPhotos.forEach(({url, comment, likes}) => {
+    const pictureElement = similarPictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__comments').textContent = comment;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    similarListFragment.appendChild(pictureElement);
+  });
+  pictures.appendChild(similarListFragment);
+};
+getPictures();
 
-const similarPictures = createPhotos;
-
-const similarListFragment = document.createDocumentFragment();
-similarPictures.forEach(({url, comment, likes}) => {
-  const pictureElement = similarPictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__comments').textContent = comment;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  similarListFragment.appendChild(pictureElement);
-});
-pictures.appendChild(similarListFragment);
-export {similarListFragment};
+export {getPictures};
